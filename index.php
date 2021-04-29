5c539f0f1a4a3d7232ae2997b34080e34625fd36
Svetlana Shmidt <svetashmidt93@gmail.com>
2021-04-29 20:27:04 +0300

    Second commit

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html>
-	<head></head>
-	<body>
-	 <table border="1" width="1000px" cellspacing="10">
-	  <tr>
-	   <th>ИМЯ</th>
-	   <td width="700px"></td>
-	   <th>ДАТА</th>
-	  </tr>
-	  <tr>
-	   <td colspan="3">Сообщение</td>
-	  </tr>
-	  <tr>
-	   <td colspan="3" height="500px"></td>
-	  </tr>
-	  <tr>
-	   <td colspan="3" align="right">
-	    <textarea cols="150"></textarea>
-	    <button>Текст</button>
-	   </td>
-	  </tr>
-	 </table>
-	</body>
+ <head></head>
+ <body>
+  <table border="1" width="1000px" cellspacing="5">
+   <tr>
+    <th>Имя</th>
+	<th width="700px"></th>
+	<th>Дата</th>
+   </tr>
+   <tr>
+    <td colspan="3">Сообщение</td>
+   </tr>
+   <tr>
+    <td colspan="3" height="500px"></td>
+   </tr>
+   <tr>
+    <td colspan="3" align="right">
+     <textarea cols="145"></textarea>
+	 <button>Нажми меня</button>
+	</td>
+   </tr>
+  </table>
+ </body>
 </html>
