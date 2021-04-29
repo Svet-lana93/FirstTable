8cd720655a2d6b1f8f7ce86f5068f2099cd70fc5
Svetlana Shmidt <svetashmidt93@gmail.com>
2021-04-29 19:24:47 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head></head>
+	<body>
+	 <table border="1" width="1000px" cellspacing="10">
+	  <tr>
+	   <th>ИМЯ</th>
+	   <td width="700px"></td>
+	   <th>ДАТА</th>
+	  </tr>
+	  <tr>
+	   <td colspan="3">Сообщение</td>
+	  </tr>
+	  <tr>
+	   <td colspan="3" height="500px"></td>
+	  </tr>
+	  <tr>
+	   <td colspan="3" align="right">
+	    <textarea cols="150"></textarea>
+	    <button>Текст</button>
+	   </td>
+	  </tr>
+	 </table>
+	</body>
+</html>
