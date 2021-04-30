2c53d055d75422dc28ac44f7506c33d80166d692
Svetlana Shmidt <svetashmidt93@gmail.com>
2021-04-30 19:32:32 +0300

    Fourth commit

--- index.php
+++ index.php
@@ -5,20 +5,19 @@
   <table width="1000px" border="1" cellspacing="5">
    <tr>
     <th>Имя</th>
-	<th width="700px"></th>
+	<td width="700px"></td>
 	<th>Дата</th>
    </tr>
    <tr>
     <td colspan="3">Сообщение</td>
    </tr>
    <tr>
-    <td height="500" colspan="3"></td>
+    <td colspan="3" height="500px"></td>
    </tr>
    <tr>
     <td colspan="3" align="right">
-     <textarea cols="150"></textarea>
+	 <textarea cols="150"></textarea>
 	 <button>Нажми меня</button>
-	</td>
    </tr>
   </table>
  </body>
